69a014e9bc171d2a56a7aaaf3028bc714018160d
ygor <ygorthiagoff@outlook.com>
2025-02-19 23:31:42 +0100

    add database

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,32 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
 
 /**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
+ * Cache the database connection in development. 
+ * This avoids creating a new connection on every HMR update.
  */
 const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
+  conn: Pool | undefined;
 };
-
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+  
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+conn.addListener("error", (err) => {
+  console.error("Database connection error:", err);
+});
 
 export const db = drizzle(conn, { schema });
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,8 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
+import { int, text, singlestoreTable, bigint } from
+"drizzle-orm/singlestore-core";
 
-import { sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
-
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `rogy-drive${name}`);
-
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const users = singlestoreTable("users_table", {
+  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
+  name: text("name"),
+  age: int("age"),
+});
